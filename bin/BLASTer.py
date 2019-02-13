211805246364684582d0ead051550f387daa1867
Ethan Holleman <eholleman@luc.edu>
2019-02-12 21:10:33 -0600

    formating changes

---
+++ bin/BLASTer.py
@@ -0,0 +1,12 @@
+
+def createBlastBD(): #Will create local BLASTn database to search elements in
+
+
+
+def searchBlastDB(): #searches 1 element in the local BLASTn database
+
+    #Method needs to return the blast output in a useable format
+    #looking for the e value start and end nucleotide and possiblely other stuff
+
+    #return a list with a predictable format of desired info
+    #or add all blast searches to a file (maybe two methods 1 for each)
