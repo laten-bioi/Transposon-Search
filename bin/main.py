114cb7688c0051bf9887dd113e3a96e74200e3bd
ethanholleman <eholleman@luc.edu>
2019-02-17 22:27:08 -0600

    method updates

--- bin/main.py
+++ bin/main.py
@@ -1,11 +1,4 @@
 def main(): #main method
-    from BLASTer import makeBlastBD, searchBlastDB
-    with open('SoyFileLocation') as db: #need to add actual file path
-        makeBlastBD('G.Max',db,'data' ) #need output location
-
-    with open('GMR30_data/GMR30_ids.fasta') as query:
-        searchBlastDB('G.Max',query,'GMR30_data/results.txt') 
-
 
 
 if __name__ == '__main__': #runs main method
--- bin/tier.py
+++ bin/tier.py
@@ -2,22 +2,22 @@ import os
 
 #assumes that the Gmax2.0 refernce genome is already good to go on PC
 
-def buildIndex(fastaPath, indexName):
-    #will give option to build index from fasta file
-
-    os.system("bowtie2-build {} -f {}").format(fastaPath, indexName)
-    os.system("echo index built")
-    return indexName
-
+def buildIndex():
+    #Builds index from a fasta file, parameters of search given in parameters.txt
+    with open('parameters.txt') as paras:
+        command = ''
+        for line in paras:
+            if line == 'INDEX':
+                search = line
+    os.system(command)
 
 def searchIndex():
     # reads command from the parameters file
     #searches updated index with the consensus seq and returns results to location in .sam
-
     with open("parameters.txt") as paras:
-        #read so that method gets correct para lines
-        #read line into command
-        search = ""
-
-        os.system(search)
+         command = ''
+         for line in paras:
+             if line == 'INDEX':
+                 command = line
+    os.system(command)
         
